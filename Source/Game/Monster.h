45e90caf7adc5e5066eec554ad9c7d3d54b3eab4
llssyy404 <christian404@naver.com>
2017-10-02 00:17:43 +0900

    Add Monster Class

---
+++ Source/Game/Monster.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+#include "Monster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_API AMonster : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
